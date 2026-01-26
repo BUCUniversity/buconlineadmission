c34bd9ec3cad9b5386b408eb0de3eb482b7cda6c
BUCUniversity <mohammedelariny565@gmail.com>
2026-01-26 19:23:01 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const menuItems = document.querySelectorAll(".menu-item");
 
 menuItems.forEach(item => {
     const page = item.getAttribute("data-page");
-
     // تنقل بين الصفحات
     item.addEventListener("click", function (e) {
     e.preventDefault();
@@ -13,28 +12,22 @@ menuItems.forEach(item => {
         window.location.href = page;
     }
     });
-
     // تفعيل العنصر الحالي
     if (page === currentPage) {
     item.parentElement.classList.add("active-menu");
     }
-
 });
 
 });
 document.addEventListener("DOMContentLoaded", function () {
-
   const currentPage = window.location.pathname.split("/").pop() || "results.html";
   const menuItems = document.querySelectorAll(".menu-item");
-
   menuItems.forEach(item => {
     const page = item.getAttribute("data-page");
-
     // تفعيل العنصر الحالي
     if (page === currentPage) {
       item.classList.add("active-link");
     }
-
     // التنقل
     item.addEventListener("click", function (e) {
       e.preventDefault();
